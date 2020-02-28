e4aed5249f9e88ec869668c72ae930ef72937bf0
Hemant <hemant.desire91@gmail.com>
2020-02-28 16:29:34 +0530

    Lambda

--- CoreJavaProj/src/com/lambdaExpression/Lambda8.java
+++ CoreJavaProj/src/com/lambdaExpression/Lambda8.java
@@ -1,10 +1,26 @@
 package com.lambdaExpression;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class Lambda8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ArrayList<Integer> ar= new ArrayList<Integer>();
+		ar.add(10);
+		ar.add(20);
+		ar.add(30);
+		ar.add(40);
+		Consumer<Integer> c=(Integer i)->System.out.println(i);
+				Runnable r=()->{
+					for(int i=0;i<ar.size();i++)
+					{
+						c.accept(ar.get(i));	
+					}
+				};
+		Thread t=new Thread(r);
+		t.start();
 	}
 
 }
