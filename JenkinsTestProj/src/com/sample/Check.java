c8a1ab85ff4f312ed0a98524cfb94febc10a8483
Hemant <hemant.desire91@gmail.com>
2020-03-09 16:34:48 +0530

    jenkins

---
+++ JenkinsTestProj/src/com/sample/Check.java
@@ -0,0 +1,7 @@
+package jarTest;
+
+public class Check {
+	public static String edit(String name){
+		 return "Hello "+name;
+		} 
+}
---
+++ JenkinsTestProj/src/com/sample/CheckMain.java
@@ -0,0 +1,11 @@
+package jarTest;
+
+public class CheckMain {
+
+	public static void main(String[] args) {
+		String s=Check.edit("Hemant");
+		System.out.println(s);
+
+	}
+
+}
